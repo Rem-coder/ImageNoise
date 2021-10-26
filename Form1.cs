e7ab5e46992b44be42cbf9b86e08fbf58a4d2f47
Rem-coder <suhomesovo306@gmail.com>
2021-10-27 00:55:23 +0500

    Добавил на форму прогресс бар, разделил рабочее место на 2 панели. Добавил отображение "%"  левом верхнем углу.

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,49 @@ namespace ImageNoise
 {
     public partial class Form1 : Form
     {
+        private SplitContainer split = new SplitContainer();
+        private TrackBar trackBarIm = new TrackBar();
+
         public Form1()
         {
-            var a = "провёл работу и готов влить в мастер";
-            InitializeComponent();
+            MyInitializeComponent();
+        }
+
+        private void MyInitializeComponent()
+        {
+            this.Text = "0%";
+            split.BorderStyle = BorderStyle.Fixed3D;
+            split.IsSplitterFixed = true;
+
+            trackBarIm.Orientation = Orientation.Vertical;
+            trackBarIm.Maximum = 100;
+            trackBarIm.Minimum = 0;
+            trackBarIm.TickStyle = TickStyle.TopLeft;
+
+            split.Panel2.Controls.Add(trackBarIm);
+            Controls.Add(split);
+
+            Load += (sender, args) => OnSizeChanged(EventArgs.Empty);
+                
+            ChageSize();
+            CheckTrackBarImValue();
+        }
+
+        private void ChageSize()
+        {
+
+            SizeChanged += (sender, args) =>
+            {
+                split.Size = new Size(ClientSize.Width, ClientSize.Height);
+                split.SplitterDistance = ClientSize.Width - 40;
+
+                trackBarIm.Size = new Size(split.Width, split.Height);
+            };
+        }
+
+        private void CheckTrackBarImValue()
+        {
+            trackBarIm.ValueChanged += (sender, args) => this.Text = trackBarIm.Value.ToString() + '%';
         }
     }
 }
